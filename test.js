4a763230bb7b150096ac89912cf0734d23d2f3b1
Keerthika <jainkeerthika006@gmail.com>
2025-03-29 06:35:12 +0530

    Added Jest and test files

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
-const assert = require("assert");
-assert.strictEqual(1 + 1, 2);
-console.log("Test Passed!");
+const sum = require('./sum');
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
 
